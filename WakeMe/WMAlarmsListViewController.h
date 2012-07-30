13422db3ea51678dee5d0e190c6e67b3434fa55d
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2012-07-30 23:51:47 +1000

    Add custom class WMAlarmsListViewController for listing alarms

---
+++ WakeMe/WMAlarmsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMAlarmsListViewController.h
+//  WakeMe
+//
+//  Created by Agustinus Verdy Widyawiradi on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMAlarmsListViewController : UITableViewController
+
+@end
