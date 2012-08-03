216399ec39dd05af424f74ca69496a90a882233b
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2012-08-03 13:58:45 +1000

    Add WMAlarmAddViewController which is subclass of UITableViewController for Alarm Add page class

---
+++ WakeMe/WMAlarmAddViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMAlarmAddViewController.h
+//  WakeMe
+//
+//  Created by Agustinus Verdy Widyawiradi on 8/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMAlarmAddViewController : UITableViewController
+
+@end
