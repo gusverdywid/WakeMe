84f51ab23a269bce157c4f4918b4b1da37d9b24b
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2012-08-07 16:19:51 +1000

    Connect the Alarm Add page with Sound Selection page

---
+++ WakeMe/WMSoundsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMSoundsListViewController.h
+//  WakeMe
+//
+//  Created by Agustinus Verdy Widyawiradi on 8/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMSoundsListViewController : UITableViewController
+
+@end
