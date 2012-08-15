70830f934f46ba1600bba5f0f556bda697f6ae3a
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2012-08-15 17:17:52 +1000

    Add Helper category for WMAlarm class entity to extend the class without changing the file directly

---
+++ WakeMe/WMAlarm+Helper.m
@@ -0,0 +1,13 @@
+//
+//  WMAlarm+Helper.m
+//  WakeMe
+//
+//  Created by Agustinus Verdy Widyawiradi on 8/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "WMAlarm+Helper.h"
+
+@implementation WMAlarm (Helper)
+
+@end
