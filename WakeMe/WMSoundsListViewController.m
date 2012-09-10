7613d206b26b645b1a6f15072503f252479a41cd
Agustinus Verdy Widyawiradi <verdywid@gmail.com>
2012-09-11 01:37:31 +1000

    Change the way it shows the number of rows of names

--- WakeMe/WMSoundsListViewController.m
+++ WakeMe/WMSoundsListViewController.m
@@ -66,7 +66,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     // Return the number of rows in the section.
-    return 1;
+    return _soundNames.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
